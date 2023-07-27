5e3007034595d6cadee700cef9b31322a26658f0
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-07-27 20:49:01 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <div className="page-layout">
-
+    <div className="page-layout" style={{textAlign:'center'}}>
+      <h1>This is an app to test the new NextJS13</h1>
+      <p>We are using jsonplaceholder.typicode to fetch the posts data</p>
+      <p>Hope you enjoy it !</p>
 
     </div>
   )
