b37a22010fb9b1c113134f722617e25999472698
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-07-27 20:11:17 -0300

    Update LikeButton.jsx

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -1,4 +1,6 @@
 'use client'
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react"
 
 
@@ -6,8 +8,13 @@ export const LikeButton = ({id}) => {
     const [liked, setLiked] = useState(false);
 
     return (
-        <button onClick={() => setLiked(!liked)}>
-           {liked ? "me gusta" : "no me gusta"}
+        <button onClick={() => setLiked(!liked)} style={{
+            zIndex:'9999',
+            height:'20px',
+        }}>
+           {liked ? 
+            <FontAwesomeIcon icon={faHeart} style={{color:"red"}} title="Like post"/>
+           : <FontAwesomeIcon icon={faHeart} style={{color:"gray"}} title="Remove like"/>}
         </button>
     )
 } 
